c8120e52712be777f0f55a62bb35ba5794a830dc
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-21 18:03:00 +0530

    jwt token created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // login user
 const loginUser = async (req, res) => {
@@ -13,7 +18,7 @@ const signupUser = async (req, res) => {
     const user = await User.signup(email, password);
 
     // create a token
-    // const token = createToken(user._id);
+    const token = createToken(user._id);
 
     res.status(200).json({ email, token });
   } catch (error) {
