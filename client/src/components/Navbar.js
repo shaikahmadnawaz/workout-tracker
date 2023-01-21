98693f5220e5254c60b2229c8fb8d5edf1cd5681
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-22 00:01:22 +0530

    css perfectly done

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
+  const { user } = useAuthContext();
+
+  const handleClick = () => {
+    logout();
+  };
+
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h1>Workout Tracker</h1>
         </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+        </nav>
       </div>
     </header>
   );
