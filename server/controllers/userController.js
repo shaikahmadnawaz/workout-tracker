763d44588bb8f49cae61ea944a1005d4133a89fd
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-21 17:03:25 +0530

    bcrypt and hash implemented

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,18 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signupUser = async (req, res) => {
-  res.json({ mssg: "signup user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+
+    // create a token
+    // const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = { loginUser, signupUser };
