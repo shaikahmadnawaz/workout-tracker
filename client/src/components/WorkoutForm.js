42f281ca25a8e33113c138f1258cf9af630474a9
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-21 11:39:04 +0530

    workouts context added

--- client/src/components/WorkoutForm.js
+++ client/src/components/WorkoutForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
+
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
@@ -30,7 +33,7 @@ const WorkoutForm = () => {
       setTitle("");
       setLoad("");
       setReps("");
-      // dispatch({ type: "CREATE_WORKOUT", payload: json });
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
   };
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null);
+  const { workouts, dispatch } = useWorkoutsContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -11,12 +12,12 @@ const Home = () => {
       const json = await response.json();
 
       if (response.ok) {
-        setWorkouts(json);
+        dispatch({ type: "SET_WORKOUTS", payload: json });
       }
     };
 
     fetchWorkouts();
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="home">
