a889f7690bde62a22e6210c8677e3bd5936f4de1
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-21 12:18:37 +0530

    delete button added

--- client/src/components/WorkoutDetails.js
+++ client/src/components/WorkoutDetails.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const workoutDetails = ({ workout }) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -13,6 +28,9 @@ const workoutDetails = ({ workout }) => {
         {workout.reps}
       </p>
       <p>{workout.createdAt}</p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
     </div>
   );
 };
--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -27,6 +27,24 @@ const getWorkout = async (req, res) => {
 // create a new workout
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!load) {
+    emptyFields.push("load");
+  }
+  if (!reps) {
+    emptyFields.push("reps");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+
   // add a doc to db
   try {
     const workout = await Workout.create({ title, load, reps });
