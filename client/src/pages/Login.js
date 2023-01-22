39947424ce4b86ad3136eec3cf876a1ae65c0c40
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-22 22:07:21 +0530

    completed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     <form className="login" onSubmit={handleSubmit}>
       <h3>Log In</h3>
 
-      <label>Email address:</label>
+      <label>Email:</label>
       <input
         type="email"
         onChange={(e) => setEmail(e.target.value)}
