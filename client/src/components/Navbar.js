bd2b042c0008dfb2f32738fe11fa251d6a5d493c
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-22 12:15:12 +0530

    useLogout hook added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+  const { logout } = useLogout;
   const { user } = useAuthContext();
 
   const handleClick = () => {
