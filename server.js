9ad1811785c8460426de0f93c0ae481915081384
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-22 22:18:40 +0530

    completed

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
+const path = require("path");
 
 // express app
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 // global middleware
 app.use(express.json());
@@ -19,6 +22,10 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 // connect to database
 mongoose
